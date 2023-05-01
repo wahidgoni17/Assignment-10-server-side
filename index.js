59a6071f0855f7fe360ffc01be5cb24d18291cc0
wahidgoni17 <imwahidgoni17@gmail.com>
2023-05-02 00:13:07 +0600

    data file included

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const port = process.ENV.PORT || 5496
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const port = process.env.PORT || 5496;
+const chef = require('./data/chef.json')
 
-app.get('/', (req, res)=>{
-    res.send("all data will be come here")
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("all data will be come here");
+});
+
+app.get("/chef", (req, res)=>{
+    console.log(chef)
+    res.send(chef)
 })
-app.listen(port, ()=>{
-    console.log(`all console can be see here ${port}` )
-})
+
+app.listen(port, () => {
+  console.log(`all console can be see here ${port}`);
+});
