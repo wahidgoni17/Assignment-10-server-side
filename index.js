0136fe98fcb82e78c74b7572ed8e7a602c46a956
wahidgoni17 <imwahidgoni17@gmail.com>
2023-05-02 11:37:42 +0600

    food data added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 5496;
 const chef = require('./data/chef.json')
+const food = require('./data/food.json')
 
 app.use(cors());
 
@@ -11,8 +12,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/chef", (req, res)=>{
-    console.log(chef)
     res.send(chef)
+})
+
+app.get("/food", (req, res)=>{
+    res.send(food)
+})
+
+app.get('/chef/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+      res.send(chef)
+  }
+  else {
+      const chefInfo = chef.filter(cf => parseInt(cf.id) === id);
+      res.send(chefInfo)
+  }
+
 })
 
 app.listen(port, () => {
